01e44d14ecaaa096495f68b021993076c29a2e00
nedaashkan <117584245+nedaashkan@users.noreply.github.com>
2023-02-10 10:07:10 +0330

    made the HTML and CSS

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// input-el
+// button-el
+// sound-el
+// word-el
+// word-type-el
+// pronouns-el
+// meaning-el
+// example-el
+//  axios;
+// use api winch it have these information
+// fix the button be responsive
